891f9e356791ef865e8d4cd14e3362c3f551e658
JimanP <qkrwlaks0909@naver.com>
2024-12-25 18:00:50 +0900

    사이드바 주문/배송, 정산, 고객관리 삽입

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,8 +16,8 @@ const Sidebar = ({ isOpen }) => {
     sidebar: {
       position: "fixed",
       top: "60px", // 헤더 아래 배치
-      left: isOpen ? "0" : "-300px", // 열고 닫기 동작
-      width: "300px",
+      left: isOpen ? "0" : "-200px", // 열고 닫기 동작
+      width: "200px",
       height: "100%",
       backgroundColor: "#ffffff",
       borderRight: "1px solid #ddd",
@@ -31,7 +31,7 @@ const Sidebar = ({ isOpen }) => {
       justifyContent: "space-between",
       alignItems: "center",
       cursor: "pointer",
-      fontSize: "25px",
+      fontSize: "20px",
       color: "#333",
       borderBottom: "1px solid #ddd",
     },
@@ -107,9 +107,77 @@ const Sidebar = ({ isOpen }) => {
           </div>
         )}
 
-        {/* 더 많은 카테고리 추가 가능 */}
+        <div style={styles.category} onClick={() => toggleExpand("주문/배송")}>
+          <div>
+            <img src="delivery.png" alt="주문/배송" style={styles.icon} />
+            주문/배송
+          </div>
+          <span
+            style={{
+              ...styles.arrow,
+              ...(expanded["주문/배송"] ? styles.arrowExpanded : {}),
+            }}
+          >
+            ▶
+          </span>
+        </div>
+        {expanded["주문/배송"] && (
+          <div style={styles.subMenu}>
+            <div style={styles.subMenuItem}>배송 관리</div>
+            <div style={styles.subMenuItem}>출고중지 요청</div>
+            <div style={styles.subMenuItem}>반품 관리</div>
+            <div style={styles.subMenuItem}>교환 관리</div>
+            <div style={styles.subMenuItem}>주문 조회</div>
+          </div>
+        )}
+
+        <div style={styles.category} onClick={() => toggleExpand("정산")}>
+          <div>
+            <img src="adjustment.png" alt="정산" style={styles.icon} />
+            정산
+          </div>
+          <span
+            style={{
+              ...styles.arrow,
+              ...(expanded["정산"] ? styles.arrowExpanded : {}),
+            }}
+          >
+            ▶
+          </span>
+        </div>
+        {expanded["정산"] && (
+          <div style={styles.subMenu}>
+            <div style={styles.subMenuItem}>정산 내역</div>
+            <div style={styles.subMenuItem}>매출 내역</div>
+            <div style={styles.subMenuItem}>부가세 신고내역</div>
+            <div style={styles.subMenuItem}>지급 내역</div>
+          </div>
+        )}
+
+        <div style={styles.category} onClick={() => toggleExpand("고객관리")}>
+          <div>
+            <img src="customer.png" alt="고객관리" style={styles.icon} />
+            고객관리
+          </div>
+          <span
+            style={{
+              ...styles.arrow,
+              ...(expanded["고객관리"] ? styles.arrowExpanded : {}),
+            }}
+          >
+            ▶
+          </span>
+        </div>
+        {expanded["고객관리"] && (
+          <div style={styles.subMenu}>
+            <div style={styles.subMenuItem}>고객문의</div>
+            <div style={styles.subMenuItem}>상품평</div>
+          </div>
+        )}
       </div>
     </div>
+    
+    
   );
 };
 
